6b29e907f3b6dc89392730f61d535817e61e06cf
Hashim-47 <hashim-47@Ubuntu.myguest.virtualbox.org>
2023-02-08 12:28:10 +0000

    Task 6 refactor

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,26 @@
-import React from "react";
-const Comment = ({ comment }) => {
+import React, { useState, useEffect } from "react";
+import { getComments } from "../utils/api";
+
+const Comment = ({ article_id }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(article_id).then((comments) => setComments(comments));
+  }, [article_id]);
+
   return (
-    <div className="comment">
-      <h4>{comment.author}</h4>
-      <p>{comment.body}</p>
+    <div className="comments">
+      <h3>Comments:</h3>
+      {comments.length === 0 ? (
+        <p>No comments</p>
+      ) : (
+        comments.map((comment) => (
+          <div className="comment" key={comment.comment_id}>
+            <h4>{comment.author}</h4>
+            <p>{comment.body}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,27 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getArticle, getComments } from "../utils/api";
+import { getArticle } from "../utils/api";
 import Comment from "./Comment";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
-  const [comments, setComments] = useState([]);
   const { article_id } = useParams();
 
   useEffect(() => {
     getArticle(article_id).then((article) => setArticle(article));
-    getComments(article_id).then((comments) => setComments(comments));
   }, [article_id]);
 
   return (
     <div className="single-article">
       <h2>{article.title}</h2>
       <p>{article.body}</p>
-      <h3>Comments:</h3>
-
-      {comments.map((comment) => (
-        <Comment key={comment.comment_id} comment={comment} />
-      ))}
+      <Comment article_id={article_id} />
     </div>
   );
 };
