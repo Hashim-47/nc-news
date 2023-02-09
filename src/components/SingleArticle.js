ba10b6faac088bd9c48cd524a22e138110c26067
Hashim-47 <hashim-47@Ubuntu.myguest.virtualbox.org>
2023-02-09 15:21:58 +0000

    Task 7 Vote on Article

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../utils/api";
+import Votes from "./Votes";
 import Comment from "./Comment";
 
 const SingleArticle = () => {
@@ -15,6 +16,7 @@ const SingleArticle = () => {
     <div className="single-article">
       <h2>{article.title}</h2>
       <p>{article.body}</p>
+      <Votes article_id={article_id} votes={article.votes} />
       <Comment article_id={article_id} />
     </div>
   );
